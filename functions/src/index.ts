a673cd59c7939caa6b42dec546a169925f32dfd7
Feiyang1 <plane1113@gmail.com>
2020-12-30 11:26:12 -0800

    set up scheduled function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as firebase from 'firebase-admin';
 import fetch from 'node-fetch';
-import {createTransport, SendMailOptions} from 'nodemailer';
-import { firestore } from 'firebase-admin';
+import { createTransport, SendMailOptions } from 'nodemailer';
 
 firebase.initializeApp();
 
@@ -10,45 +9,54 @@ const transporter = createTransport({
     service: 'gmail',
     auth: {
         user: functions.config().email.address,
-        pass: functions.config().email.pwd
-    }
+        pass: functions.config().email.pwd,
+    },
 });
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const runAlerts = functions.https.onRequest(async (request, response) => {
+export const runAlertsOnce = functions.https.onRequest(async (request, response) => {
     functions.logger.info("Hello logs!", { structuredData: true });
 
+    await runAlerts();
+
+    response.send("Hello from Firebase!");
+});
+
+export const secheduledAlerts = functions.pubsub.schedule('every 5 minutes').onRun(async (context) => {
+    console.log('This will be run every 5 minutes!');
+    await runAlerts();
+});
+
+async function runAlerts(): Promise<void> {
     const alerts = await getAlerts();
     const symbolIds = getDistinctSymbolIds(alerts);
 
     const prices = await getPrices(symbolIds);
 
     // trigger alerts
-    for(const alert of alerts) {
+    for (const alert of alerts) {
         const currentPrice = prices[alert.id]?.price;
 
-        switch(alert.type) {
+        switch (alert.type) {
             case 'above':
-                if(currentPrice >= alert.price) {
+                if (currentPrice >= alert.price) {
                     await sendAlert(alert, currentPrice);
-                    updateTriggered(alert);
+                    updateTriggered(alert).catch(err => console.log('updating triggered failed:', err));
                 }
                 break;
             case 'below':
-                if(currentPrice <= alert.price) {
+                if (currentPrice <= alert.price) {
                     await sendAlert(alert, currentPrice);
-                    updateTriggered(alert);
+                    updateTriggered(alert).catch(err => console.log('updating triggered failed:', err));
                 }
                 break;
             default:
                 throw Error(`impossible alert type: ${alert.type}`);
         }
     }
-
-    response.send("Hello from Firebase!");
-});
+}
 
 function sendAlert(alert: Alert, currentPrice: number): void {
     const message = generateAlertMessage(alert, currentPrice);
@@ -59,25 +67,25 @@ function sendAlert(alert: Alert, currentPrice: number): void {
         from: 'The awesome guy',
         to: alert.sendTo,
         subject: 'Crypto Alert',
-        html: `<p>${message}</p>`
+        html: `<p>${message}</p>`,
     }
 
 
     transporter.sendMail(mailOptions, (err, info) => {
-        if(err) {
+        if (err) {
             console.log('sending email failed', err);
         }
     });
 }
 
-function updateTriggered(alert: Alert): Promise<firestore.WriteResult> {
+function updateTriggered(alert: Alert): Promise<firebase.firestore.WriteResult> {
     return alert.docRef.update({
-        triggered: true
+        triggered: true,
     });
 }
 
 function generateAlertMessage(alert: Alert, currentPrice: number): string {
-    switch(alert.type) {
+    switch (alert.type) {
         case 'above':
             return `${alert.symbol} went over ${alert.price} at ${currentPrice}`;
         case 'below':
@@ -104,7 +112,7 @@ async function getAlerts(): Promise<Alert[]> {
                         price: alert.price,
                         type: alert.type,
                         sendTo: email,
-                        docRef: doc.ref
+                        docRef: doc.ref,
                     });
                 }
             })
@@ -117,7 +125,7 @@ async function getAlerts(): Promise<Alert[]> {
 
 function getDistinctSymbolIds(alerts: Alert[]): Set<string> {
     const distinctSymbols = new Set<string>();
-    for(const alert of alerts) {
+    for (const alert of alerts) {
         // use id since the api for querying price takes id instead of symbol
         distinctSymbols.add(alert.id);
     }
@@ -136,12 +144,12 @@ async function getPrices(ids: Set<string>): Promise<Record<string, PriceInfo>> {
     const json = await response.json();
 
     const priceMap: Record<string, PriceInfo> = {};
-    if(json.data) {
-        for(const price of json.data as CoinCapAsset[]) {
+    if (json.data) {
+        for (const price of json.data as CoinCapAsset[]) {
             priceMap[price.id] = {
                 symbol: price.symbol,
                 id: price.id,
-                price: Number(price.priceUsd)
+                price: Number(price.priceUsd),
             };
         }
     }
@@ -173,7 +181,7 @@ interface Alert {
     price: number;
     type: 'above' | 'below',
     sendTo: string;
-    docRef: firestore.DocumentReference; // It will be used to update `triggered` field after an alert is triggered
+    docRef: firebase.firestore.DocumentReference; // It will be used to update `triggered` field after an alert is triggered
 }
 
 interface AlertFirestore {
@@ -184,7 +192,7 @@ interface AlertFirestore {
     triggered: boolean
 }
 
-interface CoinCapAsset         {
+interface CoinCapAsset {
     "id": string,
     "rank": string,
     "symbol": string,
