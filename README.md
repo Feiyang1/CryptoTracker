716e656b14317087d23abfe3c9ca10372a83c3a2
Feiyang1 <plane1113@gmail.com>
2020-12-29 21:30:31 -0800

    send email to subscribers

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ TODOs:
     - Filter for triggered alerts
  - Backend
     - Query price for symbols with alerts every X mins
-    - Send email to subscribers when price target is hit
+    - ~~Send email to subscribers when price target is hit~~
+    - ~~Set `triggered` to `true` after an alert is triggered~~
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,91 @@
 import * as functions from 'firebase-functions';
 import * as firebase from 'firebase-admin';
 import fetch from 'node-fetch';
+import {createTransport, SendMailOptions} from 'nodemailer';
+import { firestore } from 'firebase-admin';
+
+firebase.initializeApp();
+
+const transporter = createTransport({
+    service: 'gmail',
+    auth: {
+        user: functions.config().email.address,
+        pass: functions.config().email.pwd
+    }
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const runAlerts = functions.https.onRequest((request, response) => {
+export const runAlerts = functions.https.onRequest(async (request, response) => {
     functions.logger.info("Hello logs!", { structuredData: true });
+
+    const alerts = await getAlerts();
+    const symbolIds = getDistinctSymbolIds(alerts);
+
+    const prices = await getPrices(symbolIds);
+
+    // trigger alerts
+    for(const alert of alerts) {
+        const currentPrice = prices[alert.id]?.price;
+
+        switch(alert.type) {
+            case 'above':
+                if(currentPrice >= alert.price) {
+                    await sendAlert(alert, currentPrice);
+                    updateTriggered(alert);
+                }
+                break;
+            case 'below':
+                if(currentPrice <= alert.price) {
+                    await sendAlert(alert, currentPrice);
+                    updateTriggered(alert);
+                }
+                break;
+            default:
+                throw Error(`impossible alert type: ${alert.type}`);
+        }
+    }
+
     response.send("Hello from Firebase!");
 });
+
+function sendAlert(alert: Alert, currentPrice: number): void {
+    const message = generateAlertMessage(alert, currentPrice);
+    console.log(message);
+
+
+    const mailOptions: SendMailOptions = {
+        from: 'The awesome guy',
+        to: alert.sendTo,
+        subject: 'Crypto Alert',
+        html: `<p>${message}</p>`
+    }
+
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if(err) {
+            console.log('sending email failed', err);
+        }
+    });
+}
+
+function updateTriggered(alert: Alert): Promise<firestore.WriteResult> {
+    return alert.docRef.update({
+        triggered: true
+    });
+}
+
+function generateAlertMessage(alert: Alert, currentPrice: number): string {
+    switch(alert.type) {
+        case 'above':
+            return `${alert.symbol} went over ${alert.price} at ${currentPrice}`;
+        case 'below':
+            return `${alert.symbol} went below ${alert.price} at ${currentPrice}`;
+        default:
+            throw Error(`impossible alert type: ${alert.type}`);
+    }
+}
 
 async function getAlerts(): Promise<Alert[]> {
     const alerts: Alert[] = [];
@@ -26,7 +103,8 @@ async function getAlerts(): Promise<Alert[]> {
                         id: alert.id,
                         price: alert.price,
                         type: alert.type,
-                        sendTo: email
+                        sendTo: email,
+                        docRef: doc.ref
                     });
                 }
             })
@@ -95,6 +173,7 @@ interface Alert {
     price: number;
     type: 'above' | 'below',
     sendTo: string;
+    docRef: firestore.DocumentReference; // It will be used to update `triggered` field after an alert is triggered
 }
 
 interface AlertFirestore {
